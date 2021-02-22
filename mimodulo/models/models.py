1a73fa3d0ec3746b06cc3d7f26591f26f4fe11d7
root <root@fdd772d8f367>
2021-02-22 11:55:31 +0000

    Creando clases

--- mimodulo/models/models.py
+++ mimodulo/models/models.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
-# class mimodulo(models.Model):
-#     _name = 'mimodulo.mimodulo'
+class Helado(models.Model):
+    _name = 'helados.receta'
+    _order = 'sabor desc, name'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
+    sabor = fields.Char('Sabor', required=True)
+    estado = fields.Integer()
+    data_alta = fields.Date('Data de alta')
+    state = fields.Selection(
+        [('desarrollo', 'Desarrollo'),
+         ('experimental', 'Experimental'),
+         ('producción', 'En producción')],
+        'State', default="desarrollo")
+    temperatura_celsius=fields.Float("Temperatura Congelación", required=True)
+    value2 = fields.Float(compute="_value_pc", store=True)
+    receta = fields.Text("Receta", required=True)
+    autor = fields.Many2one('res.currency', string='Autor')
+
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
